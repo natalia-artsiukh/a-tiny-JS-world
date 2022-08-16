d5a9b7d0c0f23fc0527ee03c61a7d9f90916ca39
Natalia Artsiukh <nataliaprocessmail@gmail.com>
2022-08-16 23:22:01 +0300

    Converted message from string concatenation to template. Arrow function argument changed to object destructuring. Unnecessary return deleted. Main code moved to index.js, lib.js restored to default.

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
+const man = {
+   hands: 2,
+   legs: 2,
+   name: 'Bob',
+   gender: 'male',
+   saying: 'I\'m vegan',
+   friends: 'no one'
+ };
+ const woman = {
+   hands: 2,
+   legs: 2,
+   name: 'Mary',
+   gender: 'female',
+   saying: 'Hi everyone',
+   friends: 'Tom, Rex, Halle'
+ };
+ const cat = {
+   hands: 0,
+   legs: 4,
+   name: 'Tom',
+   gender: 'male',
+   saying: 'Mrrrr',
+   friends: 'Mary, Halle'
+ };
+ const dog = {
+   hands: 0,
+   legs: 4,
+   name: 'Rex',
+   gender: 'male',
+   saying: 'Woof',
+   friends: 'Mary'
+ };
+ const catwoman = {
+   __proto__:cat,
+   hands: 2,
+   legs: 2,
+   name: 'Halle',
+   gender: 'female',
+   friends: 'Mary, Tom, Batman'
+ };
+ 
+ const habitants = [man, woman, cat, dog, catwoman];
+ 
+ habitants.forEach(({ saying, name, gender, hands, legs, friends }) => print(`${saying}! My name is ${name}. I'm ${gender}. I have ${hands} hands and ${legs} legs. I'm friends with ${friends}.`));
 
